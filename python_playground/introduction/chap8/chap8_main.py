8c6498182a7b9b88a9b2831057657fed1db476d2
Jerry Li <53632783+jerli4@users.noreply.github.com>
2024-02-22 23:25:15 +0800

    Learned dictionary.

---
+++ python_playground/introduction/chap8/chap8_main.py
@@ -0,0 +1,13 @@
+from python_playground.introduction.chap8.learn_dictionaries import learn_iterate_dictionary_keys, \
+    learn_iterate_dictionary_values, learn_iterate_dictionary_first, learn_iterate_dictionary_second
+
+# learn_iterate_dictionary_keys()
+
+
+# learn_iterate_dictionary_values()
+
+
+# learn_iterate_dictionary_first()
+
+
+learn_iterate_dictionary_second()
